38c119bba344321add5d46a750d4079c58530d5d
Erika <146768635+erigt@users.noreply.github.com>
2023-12-09 22:16:09 +0100

    App.java

--- src/main/java/dev/erika/ejercicio1_java_class/App.java
+++ src/main/java/dev/erika/ejercicio1_java_class/App.java
@@ -7,6 +7,7 @@ public final class App {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello World!");
+        Person mari = new Person("Mari", "Limones", "01234567R", 1960);
+        Person yoli = new Person("Yoli", "Naranjas", "89012345Y", 1980);
     }
 }
