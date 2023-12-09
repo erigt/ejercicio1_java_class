168beed9ac778c648c2278416d3e08fb4714a598
Erika <146768635+erigt@users.noreply.github.com>
2023-12-09 10:03:21 +0100

    readme

--- src/main/java/dev/erika/ejercicio1_java_class/App.java
+++ src/main/java/dev/erika/ejercicio1_java_class/App.java
@@ -1,17 +1,12 @@
 package dev.erika.ejercicio1_java_class;
 
-/**
- * Hello world!
- */
 public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+
     public static void main(String[] args) {
+        
         System.out.println("Hello World!");
     }
 }
