d305e439b32780a48f344b93ea8e7a8bb1ee0460
Erika <146768635+erigt@users.noreply.github.com>
2023-12-09 09:52:33 +0100

    Exercise1 Java_Class

---
+++ src/test/java/dev/erika/ejercicio1_java_class/AppTest.java
@@ -0,0 +1,18 @@
+package dev.erika.ejercicio1_java_class;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
